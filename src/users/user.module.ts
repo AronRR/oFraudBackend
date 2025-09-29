beb945cd8311b96c84d05c7599df209a0b4ada62
AronRR <A01666725@tec.mx>
2025-09-28 21:36:35 -0600

    Errors fixed

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -1,13 +1,16 @@
 /* eslint-disable prettier/prettier */
 
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
+import { AuthModule } from "src/auth/auth.module";
+import { JwtAuthGuard } from "src/common/guards/jwt-auth.guard";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { UserRepository } from "./user.repository";
 
 @Module({
+  imports: [forwardRef(() => AuthModule)],
   controllers: [UserController],
-  providers: [UserRepository,UserService],
+  providers: [UserRepository, UserService, JwtAuthGuard],
   exports: [UserService]
 })
 export class UserModule {}
