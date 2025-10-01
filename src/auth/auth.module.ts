24d8c4d39f26433e9656fd39c8c1d44f2338a59f
AronRR <A01666725@tec.mx>
2025-09-30 21:11:33 -0600

    Fix errors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,15 @@
 /* eslint-disable prettier/prettier */
 
 import { Module, forwardRef } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
 import { UserModule } from "src/users/user.module";
+import { jwtConfig } from "src/config/jwt.config";
 import { AuthController } from "./auth.controller";
 import { TokenService } from "./tokens.service";
 
 
 @Module({
-    imports: [forwardRef(() => UserModule)],
+    imports: [forwardRef(() => UserModule), JwtModule.registerAsync(jwtConfig)],
     controllers: [AuthController],
     providers: [TokenService],
     exports: [TokenService]
