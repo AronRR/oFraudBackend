cdabcdf38eee081cf43c25f1e5f8a2f6f81907bc
AronRR <A01666725@tec.mx>
2025-10-06 16:57:13 -0600

    added documentation by Scalar

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+@ApiTags('Health')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Verificar estado de la API' })
+  @ApiOkResponse({
+    description: 'API funcionando correctamente',
+    schema: {
+      type: 'string',
+      example: 'Hello World!'
+    }
+  })
   getHello(): string {
     return this.appService.getHello();
   }
